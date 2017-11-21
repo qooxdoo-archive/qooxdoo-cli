7e4d745971d0977df8d1457ba89a2fa8d2bf04b3
Christian M. Boulanger <info@bibliograph.org>
2017-11-22 00:06:04 +0100

    Added create_ command, not yet ready for production

---
+++ commands/create_.js
@@ -0,0 +1,22 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2017 Christian Boulanger
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Boulanger (info@bibliograph.org, @cboulanger)
+
+************************************************************************ */
+
+/*global qxcli*/
+
+require("../lib/qxcli/commands/contrib/Create");
+module.exports = qxcli.commands.contrib.Create.getYargsCommand();
---
+++ foo4/source/class/foo4/__init__.js
@@ -0,0 +1,6 @@
+/** <h3> foo4 API Documentation </h3>
+ *
+ * Replace this text with an appropriate overview and introduction to your 
+ * application.
+ *
+ */
