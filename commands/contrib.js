81df63adf600cf43ba3f897152d06c7c859f30c4
Henner Kollmann <henner.kollmann@gmx.de>
2017-12-07 13:07:51 +0100

    unified the command so that  they all use the same syntax    require("../lib/qxcli/commands/Upgrade");    module.exports = qxcli.commands.Upgrade.getYargsCommand();
    issue #35: Streamline options: All contrib commands do have there own getYargsCommand

--- commands/contrib.js
+++ commands/contrib.js
@@ -1,25 +1,3 @@
 exports.command = "contrib <command> [options]";
 exports.desc = "manages qooxdoo contrib libraries";
-exports.builder = function (yargs) {
-  return yargs
-    .commandDir('contrib_commands')
-    .option('T', {
-        alias: 'token',
-        describe: 'Use a GitHub access token'
-    })
-    .option('f', {
-        alias: 'file',
-        describe: 'Output result to a file'
-    })       
-    .option('v', {
-        alias: 'verbose',
-        describe: 'Verbose logging'
-    })
-    .option('q', {
-        alias: 'quiet',
-        describe: 'No output'
-    })  
-    .demandCommand()
-    .showHelpOnFail()
-}
 exports.handler = function (argv) {}
