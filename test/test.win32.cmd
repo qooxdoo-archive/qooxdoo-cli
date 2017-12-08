d761bd0a6969c06ba146041e2879ece2d21355b5
Henner Kollmann <henner.kollmann@gmx.de>
2017-12-08 21:50:43 +0100

    #46:  Consistent error handling

--- test/test.win32.cmd
+++ test/test.win32.cmd
@@ -1,12 +1,12 @@
 rmdir  /Q /S myapp
-call qx create myapp -I 
+call qx create myapp -I  || EXIT /B 1
 cd myapp
-call qx contrib update
-call qx contrib list
-call qx contrib install oetiker/UploadWidget --release v1.0.0
-call qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3
-call qx contrib install johnspackman/UploadMgr --release v1.0.0
-call qx compile
+call qx contrib update || EXIT /B 1
+call qx contrib list || EXIT /B 1
+call qx contrib install oetiker/UploadWidget --release v1.0.0 || EXIT /B 1
+call qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3 || EXIT /B 1
+call qx contrib install johnspackman/UploadMgr --release v1.0.0 || EXIT /B 1
+call qx compile || EXIT /B 1
 rmdir /Q /S contrib
-call qx contrib install 
-call qx contrib remove cboulanger/qx-contrib-Dialog
+call qx contrib install  || EXIT /B 1
+call qx contrib remove cboulanger/qx-contrib-Dialog || EXIT /B 1
