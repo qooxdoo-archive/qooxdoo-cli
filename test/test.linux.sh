ea2716b0d6917a05a6cc67ca5c90fbeaea1acc81
Henner Kollmann <henner.kollmann@gmx.de>
2017-12-11 11:46:23 +0100

    fix manifest path for absolute paths

--- test/test.linux.sh
+++ test/test.linux.sh
@@ -1,13 +1,13 @@
 set -x 
 rm -rf myapp
-qx create myapp -I || exit $?
+qx create myapp -I -V || exit $?
 cd myapp
-qx contrib update  || exit $?
-qx contrib list    || exit $?
-qx contrib install oetiker/UploadWidget --release v1.0.0  || exit $?
-qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3  || exit $?
-qx contrib install johnspackman/UploadMgr --release v1.0.0  || exit $?
-qx compile  || exit $?
+qx contrib update  -V|| exit $?
+qx contrib list    -V|| exit $?
+qx contrib install oetiker/UploadWidget --release v1.0.0 -V || exit $?
+qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3 -V || exit $?
+qx contrib install johnspackman/UploadMgr --release v1.0.0 -V || exit $?
+qx compile -V || exit $?
 rm -rf contrib  || exit $?
-qx contrib install || exit $?
-qx contrib remove cboulanger/qx-contrib-Dialog || exit $?
+qx contrib install -V || exit $?
+qx contrib remove cboulanger/qx-contrib-Dialog -V || exit $?
--- test/test.win32.cmd
+++ test/test.win32.cmd
@@ -1,12 +1,12 @@
 rmdir  /Q /S myapp
-call qx create myapp -I  || EXIT /B 1
+call qx create myapp -I  -V || EXIT /B 1
 cd myapp
-call qx contrib update || EXIT /B 1
-call qx contrib list || EXIT /B 1
-call qx contrib install oetiker/UploadWidget --release v1.0.0 || EXIT /B 1
-call qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3 || EXIT /B 1
-call qx contrib install johnspackman/UploadMgr --release v1.0.0 || EXIT /B 1
-call qx compile || EXIT /B 1
+call qx contrib update -V || EXIT /B 1
+call qx contrib list -V || EXIT /B 1
+call qx contrib install oetiker/UploadWidget --release v1.0.0 -V || EXIT /B 1
+call qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3 -V || EXIT /B 1
+call qx contrib install johnspackman/UploadMgr --release v1.0.0 -V || EXIT /B 1
+call qx compile -V || EXIT /B 1
 rmdir /Q /S contrib
-call qx contrib install  || EXIT /B 1
-call qx contrib remove cboulanger/qx-contrib-Dialog || EXIT /B 1
+call qx contrib install  -V || EXIT /B 1
+call qx contrib remove cboulanger/qx-contrib-Dialog -V || EXIT /B 1
