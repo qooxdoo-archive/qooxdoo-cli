f84120e9b22de2eec9565180af9d590ce41642b9
Christian M. Boulanger <info@bibliograph.org>
2018-01-08 17:41:52 +0100

    added support for static header for all templates

--- templates/class/interface.tmpl.js
+++ templates/class/interface.tmpl.js
@@ -1,20 +1,4 @@
-/* ************************************************************************
-
-  ${name}
-
-  ${homepage}
-
-  Copyright: 
-    ${copyright}
-
-  License: 
-    ${license}
-    See the LICENSE file in the project's top-level directory for details.
-
-  Authors: 
-    ${authors}
-
-************************************************************************ */
+${header}
 
 /**
  * This is a qooxdoo interface
