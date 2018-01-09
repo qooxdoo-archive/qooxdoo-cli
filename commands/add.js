7aafc54749ae342e7660e99e995f2c6a5aaf0ceb
Christian M. Boulanger <info@bibliograph.org>
2018-01-09 13:34:46 +0100

    Made qx add a super command like qx contrib

--- commands/add.js
+++ commands/add.js
@@ -1,19 +1,2 @@
-/* ************************************************************************
-
-   qooxdoo - the new era of web development
-
-   http://qooxdoo.org
-
-   Copyright:
-     2017 Christian Boulanger
-
-   License:
-     MIT: https://opensource.org/licenses/MIT
-     See the LICENSE file in the project's top-level directory for details.
-
-   Authors:
-     * Christian Boulanger (info@bibliograph.org, @cboulanger)
-
-************************************************************************ */
 require("../lib/qxcli/commands/Add");
 module.exports = qxcli.commands.Add.getYargsCommand();
